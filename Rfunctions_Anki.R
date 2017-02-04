999c4f1737e7f6f247fa443508b52787c04dd282
J. Allen Baron <jabaron.phd@gmail.com>
2017-02-04 18:01:44 -0500

    Create script to convert R fxn docs to Anki cards (eventually)
    Purpose: To programmatically create Anki cards from the documentation files of individual functions, either from all packages currently available or only from specified packages.
    
    Unresolved issues:
    1. html selection may not work for documentation of all functions
    	- definitely need to avoid package documentation when selecting functions
    
    * This might work better using readLines instead of html notation.

---
+++ Rfunctions_Anki.R
@@ -0,0 +1,82 @@
+# Automatically add man pages for functions from specified package
+# Packages are located in /Library/Frameworks/R.framework/Versions/3.3/Resources/library/
+
+# To do:
+#   1. Find location & format of pkg man pages
+#   2. Determine how libraries can be automatically added to ANKI (saw this on mobile app
+#       in play store)
+
+# INDEX = /Library/Frameworks/R.framework/Versions/3.3/Resources/library/dplyr/INDEX
+#   but no links in txt file
+
+###### USE HTML VERSIONS I CAME ACROSS ##########
+library(rvest)
+
+# computer connection to open html (not sure what this is exactly)
+connection <- "http://127.0.0.1:11435/"
+
+# html listing all packages
+Rpkg_index <- "doc/html/packages.html"
+Rpkg_selector <- "td a"
+Rpkg_index_html <- read_html(paste(connection, Rpkg_index, sep = ""))
+
+# list package paths, names, and parent html file (lists all functions)
+pkg_paths <- html_nodes(Rpkg_index_html, Rpkg_selector) %>%
+    html_attr('href') %>%
+    sub("(../../)(.*)(00Index.html)", "\\2", .)
+pkg_names <- sub("(^library/)(.*)(/html/$)", "\\2", pkg_paths)
+pkg_index_file <- "00Index.html"
+
+# prep df for output to anki (as csv?)
+anki <- data.frame(name = "", package = "", summary = "", description = "",
+                   usage = "", arguments = "")
+
+pos <- 0
+
+anki_cards <- function(pkgs) {
+    # Input packages desired to convert documentation to Anki cards
+    # Arguments:
+    #   pkgs: names of packages to convert to Anki cards (character vector)
+    
+    for (i in c(4, 28, 43, 71)) { #seq_along(pkg_paths)
+        # open pkg parent html
+        pkg_html <- read_html(paste(connection, pkg_path[i], pkg_index_file, sep = ""))
+        
+        # list path for all functions within given package
+        fxn_selector <- "td a"
+        fxn_paths <- html_nodes(pkg_html, fxn_selector) %>%
+            html_attr('href')
+        fxn_names <- sub("(^.*)(.html$)", "\\1", fxn_paths)
+        
+        for (j in seq_along(fxn_paths)) {
+            if (fxn_names[j] == pkg_names[i]) {
+                next
+            }
+            fxn_html <- read_html(paste(connection, pkg_path[i], fxn_paths[j], sep = ""))
+            pos <- pos + 1
+            anki$name[pos] <- fxn_names[j]
+            anki$package[pos] <- pkg_names[i]
+            anki$summary <- html_nodes(fxn_html, "h2") %>%
+                html_text()
+            anki$description <- html_nodes(fxn_html, "p:nth-child(4)") %>%
+                html_text()
+            anki$usage <- as.character(html_nodes(fxn_html, "pre:first-of-type"))
+            anki$arguments <- as.character(html_nodes(fxn_html, "[summary='R argblock']"))
+            
+            ## scrape function file (as text)
+            #fxn_info <- readLines(paste(connection, pkg_path[28], fxn_paths[160], sep = ""))
+            #description_start <- grep("Description", fxn_info) + 2
+            #description <- fxn_info[grep("Description", fxn_info) + 2]
+        }
+    }
+    
+    # post-processing
+    #   tables
+    #       1. replace "\n" with " "; some may need to be replaced with "<br>"
+    #           in order to force a break (maybe "\n\n")
+    #       2. <p> and </p> tags are OKAY!
+    #       3. fix "valign=.*>"
+    
+    # export as txt for Anki
+    # Anki can import html if it is in a plain txt format 
+    # anki_dir <- "/Users/jbaron/Library/Application Support/Anki2"
