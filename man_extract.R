404f1003510eceacd14dd51b15b52683205de48b
J. Allen Baron <jabaron.phd@gmail.com>
2017-02-11 00:47:52 -0500

    Internalize help_binding and resolve name errors in man_extract
    Implemented changes to make man_extract more similar to help():
    1. Function and package names do not need to be provided as character vectors
    2. Other arguments can be passed to help(); both 'package' and 'try.all.packages' work correctly
    3. Error if package namespace has not been loaded more similar to that returned by help(), but with added suggestions.
    4. Error from help returned if >1 name. Also, help error returned for package specifications.

--- man_extract.R
+++ man_extract.R
@@ -1,25 +1,40 @@
-man_extract <- function(help_binding,
+man_extract <- function(function_name, package = NULL, ...,
                          fields = c("Description", "Usage", "Arguments")) {
     # Extract the content (in html format) from the specified "fields",
     # as well as, the header info (function name, package name, and summary)
     # from the function man page
     # Arguments:
-    #   help_binding = e.g. x <- help(topic = "abbreviate")
+    #   function_name = should be a name, length-one character vector or reserved word
+    #   package = name of package that function is in
+    #   ... = other arguments passed to help()
     #   fields = heading names of content from man page to extract
     #       (correct capitalization necessary)
     
-    # test if required packages installed
+    # test for required packages and load
     req_pkgs <- c("rvest", "xml2", "magrittr")
     if (!all(req_pkgs %in% installed.packages()[,"Package"])) {
         stop("required pkgs (rvest, xml2, magrittr) must be installed!")
     }
-    
     library(rvest)
     library(magrittr)
-    function_name <- basename(help_binding)
-    pkg_name <- basename(dirname(dirname(help_binding)))
     
-    # reload man page as html for desired function
+    # allow for variables in name or character format
+    if (is.name(y <- substitute(function_name))) {
+        function_name <- as.character(y)
+    }
+    if (!missing(package)) {
+        if (is.name(y <- substitute(package))) {
+            package <- as.character(y)
+        }
+    }
+    
+    # load man page (e.g. help()) as html for desired function
+    help_binding <- help(topic = eval(function_name), package = eval(package), ...)
+    if (length(help_binding) < 1 & is.null(package)) {
+        stop(paste0("No documentation for ‘", function_name,
+                    "’ in specified packages and libraries:",
+                    "\n  specify package or other arguments to help()"))
+    }
     help_Rd <- utils:::.getHelpFile(help_binding)
     tools::Rd2HTML(help_Rd, out = "temp.html")
     help_html <- read_html("temp.html")
@@ -31,7 +46,9 @@ man_extract <- function(help_binding,
         gsub("\n", " ", .) %>%
         gsub('\"',"'", .)
     
-    # extract help info from help_html for card
+    # extract html man content
+    function_name <- basename(help_binding)
+    pkg_name <- basename(dirname(dirname(help_binding)))
     summary <- html_nodes(help_html, "h2") %>%
         html_text()
     h3_index <- grep("^<h3>", elements)
@@ -53,3 +70,13 @@ man_extract <- function(help_binding,
     }
     field_vals
 }
+
+z <- function(function_name) {
+    if (is.name(y <- substitute(function_name))) {
+        function_name <- as.character(y)
+    }
+    print(y)
+    print(function_name)
+    help(topic = eval(function_name))
+}
+    
