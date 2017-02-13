8112705ec92339d0a24b38a44eee62b1f53fef02
J. Allen Baron <jabaron.phd@gmail.com>
2017-02-13 13:41:09 -0500

    Adjust anki_Rcards() for updated man_extract()

--- anki_Rcards.R
+++ anki_Rcards.R
@@ -1,24 +1,28 @@
-anki_Rcards <- function(packages, ..., quietly = FALSE) {
+anki_Rcards <- function(packages, ..., warn = FALSE, programmatically = TRUE, 
+                        progress = TRUE) {
     # Extract man pages for functions from specified packages
     # Arguments:
     #   packages = character vector naming packages
-    #   ... = other arguments to man_extract() and help()
-    #   quietly = logical; display progress?
+    #   ... = other arguments to man_extract()
+    #   progress = logical; display progress?
     
-    cards <- list()
+    pkg_cards <- list()
     for (i in packages) {
         functions <- list_functions(i)
-        if (!quietly) {
+        if (progress) {
             message("'", i, "' extraction started")
         }
-        content <- lapply(functions$function_name, man_extract, 
+        
+        for (n in functions) {
+            content <- lapply(functions$function_name, man_extract, 
                           package = i, ...)
-        cards[[i]] <- do.call(rbind, content)
-        if (!quietly) {
+        }
+        pkg_cards[[i]] <- do.call(rbind, content)
+        if (progress) {
             message("COMPLETE!")
         }
     }
-    anki <- do.call(rbind, cards)
-    anki <- anki[!duplicated(anki), ]
-    anki
+    cards <- do.call(rbind, pkg_cards) %>%
+        subset(., !duplicated(.))
+    cards
 }
